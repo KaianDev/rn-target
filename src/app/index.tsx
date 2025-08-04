c744da62be27c66b6b1a8a9c3753f355ef4f7f68
KaianDev <kaianvasconcelos@gmail.com>
2025-08-03 23:38:55 -0300

    feat: add font support and update layout with custom font family

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,13 @@
 import { router } from "expo-router"
 import { Button, Text, View } from "react-native"
+import { fontFamily } from "@/theme"
 
 export default function Index() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Hello, world!</Text>
+      <Text style={{ fontFamily: fontFamily.bold, fontSize: 34 }}>
+        Hello, world!
+      </Text>
 
       <Button title="Nova meta" onPress={() => router.push("/target")} />
       <Button title="Transação" onPress={() => router.push("/transaction/1")} />
