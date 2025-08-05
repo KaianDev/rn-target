62b2fecc67d69d0400456e812d7d6b9789d2eaad
KaianDev <kaianvasconcelos@gmail.com>
2025-08-05 16:27:47 -0300

    feat: refactor Transaction component layout and integrate CurrencyInput and Input for user input

--- src/app/transaction/[id].tsx
+++ src/app/transaction/[id].tsx
@@ -1,13 +1,28 @@
-import { router, useLocalSearchParams } from "expo-router"
-import { Button, Text, View } from "react-native"
+import { StatusBar, View } from "react-native"
+import { useLocalSearchParams } from "expo-router"
+
+import { Button } from "@/components/button"
+import { PageHeader } from "@/components/page-header"
+import { CurrencyInput } from "@/components/currency-input"
+import { Input } from "@/components/input"
 
 export default function Transaction() {
   const { id } = useLocalSearchParams()
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Transaction Page</Text>
-      <Text>ID: {id}</Text>
-      <Button title="Go Back" onPress={() => router.back()} />
+    <View style={{ flex: 1, padding: 24 }}>
+      <StatusBar barStyle="dark-content" />
+      <PageHeader
+        title="Nova transação"
+        subtitle="A cada valor guardado você fica mais próximo de sua meta. Se esforce para guardar e evitar retirar."
+      />
+      <View style={{ marginTop: 32, gap: 24 }}>
+        <CurrencyInput label="Valor" value={0} />
+        <Input
+          label="Motivo (opcional)"
+          placeholder="Ex: Investir em CDB de 110% no banco XPTO"
+        />
+        <Button title="Salvar" onPress={() => {}} />
+      </View>
     </View>
   )
 }
